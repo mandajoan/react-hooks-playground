71e8420f2e957e322451b405e953aa072dec3e9b
Amanda Joan <amandajoanski@gmail.com>
2019-04-18 13:53:21 -0700

    game working

--- src/index.js
+++ src/index.js
@@ -1,52 +1,65 @@
-import React from 'react';
+import React, {useState} from 'react';
+//useState allows functional components to have state
 import ReactDOM from 'react-dom';
 import './index.css';
 
+//Starter Code:
+//https://codepen.io/gaearon/pen/oWWQNa 
 
-class Square extends React.Component {
-    render() {
+//destructuring props here
+const Square = ({value, onClick }) =>  {
+    //value and function to trigger state change -- initial state is null
+    //onClick calls setValue() and updates state
       return (
-        <button className="square">
-          {/* TODO */}
+        <button onClick={onClick} className="square">
+          { value }
         </button>
       );
-    }
   }
-  
-  class Board extends React.Component {
-    renderSquare(i) {
-      return <Square />;
+
+const Board = () => {
+    //shortcut array method to create an array of 9 nulls
+    const [ squares, setSquares ] = useState(Array(9).fill(null))
+    //use HOC to toggle between x & o
+    const [isXNext, setXNext] = useState(true)
+    //the squares component doesn't need to be aware of the value of other squares
+    //so we keep all logic here (vs passing in more info via props)
+    const renderSquare = (i) => {
+        return <Square value={squares[i]} 
+        onClick={() => {
+        //make a copy of array to update state
+        const nextSquares = squares.slice()
+        nextSquares[i] = isXNext ? 'x' : 'o'
+        setXNext(!isXNext)
+        setSquares(nextSquares) }}
+        number={i}/>
     }
-  
-    render() {
-      const status = 'Next player: X';
-  
-      return (
+    const status = 'Next player: X';
+     return (
         <div>
           <div className="status">{status}</div>
           <div className="board-row">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
+            {renderSquare(0)}
+            {renderSquare(1)}
+            {renderSquare(2)}
           </div>
           <div className="board-row">
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
+            {renderSquare(3)}
+            {renderSquare(4)}
+            {renderSquare(5)}
           </div>
           <div className="board-row">
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
+            {renderSquare(6)}
+            {renderSquare(7)}
+            {renderSquare(8)}
           </div>
         </div>
-      );
-    }
-  }
+    );
+    
+}
   
-  class Game extends React.Component {
-    render() {
-      return (
+const Game = () => {
+    return (
         <div className="game">
           <div className="game-board">
             <Board />
@@ -56,9 +69,8 @@ class Square extends React.Component {
             <ol>{/* TODO */}</ol>
           </div>
         </div>
-      );
-    }
-  }
+    );
+}
   
   // ========================================
   
